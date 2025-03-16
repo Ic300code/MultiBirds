fe8bbb53585ff95e1cc8cbf25b6e74428cd8e308
Ic300code <ifontenoygonier@gmail.com>
2025-03-16 08:38:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: ["https://multibirds.onrender.com", "https://serve.gamejolt.net"],
+        origin: ["*"],
         methods: ["GET", "POST"]
     }
 });
